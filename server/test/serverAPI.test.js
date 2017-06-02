af42407a6ebee156675568ec712bad941db9fabd
Radivarig <reslav.hollos@gmail.com>
2017-06-02 20:20:13 +0200

    Added convertMentions test Fixed missing type, imported Issue Removed unneeded async function prefix

--- server/test/serverAPI.test.js
+++ server/test/serverAPI.test.js
@@ -2,7 +2,9 @@ import chai, {expect} from 'chai'
 import {asyncTimeout} from '../src/helpers'
 import {webhookHandler} from '../src/serverAPI'
 import integrationRest from '../src/integrationRest'
-
+import {
+  Issue,
+} from '../src/types'
 
 const originalYoutrackIssue: Issue = {
   id: "TEST-1",
@@ -16,13 +18,26 @@ const mirrorOfYoutrackIssue: Issue =
 
 describe('getIsOriginal', () => {
 
-  it ('returns true if entity body does not contain meta html comment', async () => {
+  it ('returns true if entity body does not contain meta html comment', () => {
     const isOriginal = webhookHandler.getIsOriginal (originalYoutrackIssue)
     expect (isOriginal).to.equal (true)
   })
 
-  it ('returns false if entity body does contain meta html comment', async () => {
+  it ('returns false if entity body does contain meta html comment', () => {
     const isOriginal = webhookHandler.getIsOriginal (mirrorOfYoutrackIssue)
     expect (isOriginal).to.equal (false)
   })
+})
+
+describe('convertMentions', () => {
+
+  it ('breaks all mention formats', () => {
+    const allMonkeysFollowedByApopstrophe = false
+    const originalBody = "@start @middle @end\n@second_line"
+    const convertedBody = webhookHandler.convertMentions (originalBody, "")
+
+    const matches = convertedBody.match (/\B@[^']+/ig)
+    expect (matches).length.to.equal (null)
+  })
+
 })
