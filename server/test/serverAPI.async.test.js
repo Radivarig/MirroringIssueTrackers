9fc3d787489206a9a06d75667171937efdba2f29
Radivarig <reslav.hollos@gmail.com>
2017-06-06 21:46:37 +0200

    Added testing all services for project existence

--- server/test/serverAPI.async.test.js
+++ server/test/serverAPI.async.test.js
@@ -4,20 +4,28 @@ import {webhookHandler} from '../src/serverAPI'
 // import integrationRest from '../src/integrationRest'
 
 import auth from '../config/auth.config'
+import {services} from '../config/const.config'
 
 describe('projectExist', () => {
   const randomName = "temp-" + Math.random().toString(36).replace(/[^a-z0-9]+/g, '')
 
   it ('returns false for a non existing project', async () => {
-    const projExist: boolean = await webhookHandler.projectExist (randomName, "github")
-    expect (projExist).to.equal (false)
+    await Promise.all (services.map (async (service) => {
+      const projExist: boolean = await webhookHandler.projectExist (randomName, service)
+      expect (projExist).to.equal (false)
+    }))
   })
 
   it ('returns true for a known test project', async () => {
-    const projExist: boolean = await webhookHandler.projectExist (auth.github.project, "github")
-    expect (projExist).to.equal (true)
+    await Promise.all (services.map (async (service) => {
+      const projExist: boolean = await webhookHandler.projectExist (auth[service].project, service)
+      expect (projExist).to.equal (true)
+    }))
   })
+})
 
+
+describe('throwIfAnyProjectNotExist', () => {
   it ('throws if any of services test project does not exist', async () => {
     await webhookHandler.throwIfAnyProjectNotExist ()
   })
