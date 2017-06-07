ac4ef5f2cf13635917f79d0ec3f963b59beb2bbd
Radivarig <reslav.hollos@gmail.com>
2017-06-07 15:39:15 +0200

    Added getting only issues since test timestamp

--- server/test/serverAPI.async.test.js
+++ server/test/serverAPI.async.test.js
@@ -5,6 +5,9 @@ import {webhookHandler} from '../src/serverAPI'
 
 import auth from '../config/auth.config'
 import {services} from '../config/const.config'
+import {
+  Entity,
+} from '../src/types'
 
 describe('projectExist', () => {
   const randomName = "temp-" + Math.random().toString(36).replace(/[^a-z0-9]+/g, '')
@@ -24,9 +27,36 @@ describe('projectExist', () => {
   })
 })
 
-
 describe('throwIfAnyProjectNotExist', () => {
   it ('throws if any of services test project does not exist', async () => {
     await webhookHandler.throwIfAnyProjectNotExist ()
+  })
+})
+
+describe('getProjectIssues', async () => {
+  it ('returns only issues since given timestamp', async () => {
+    await Promise.all (services.map (async (service) => {
+      const issueA: Entity = {
+        id: Math.random ().toString (),
+        title: Math.random ().toString (),
+        body: Math.random ().toString (),
+        service,
+      }
+
+      const issueB: Entity = {
+        id: Math.random ().toString (),
+        title: Math.random ().toString (),
+        body: Math.random ().toString (),
+        service,
+      }
+
+      await webhookHandler.createIssue (issueA, service)
+      await helpers.asyncTimeout (5000)
+      const sinceTimestamp = new Date ().getTime ()
+      await webhookHandler.createIssue (issueB, service)
+
+      const issues = await webhookHandler.getProjectIssues (service, sinceTimestamp)
+      expect (issues.length).to.equal (1)
+    }))
   })
 })
