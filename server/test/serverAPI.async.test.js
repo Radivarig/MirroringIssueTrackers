51276b1b8890ea4094eb0429b58ad94ff73041b4
Radivarig <reslav.hollos@gmail.com>
2017-06-08 23:46:42 +0200

    Added test for comment creation

--- server/test/serverAPI.async.test.js
+++ server/test/serverAPI.async.test.js
@@ -35,29 +35,41 @@ describe('throwIfAnyProjectNotExist', () => {
 
 // get issues since now
 const testTimestamp = new Date ().getTime ()
-const randomIssues = []
 
-// create 2 issues on each service
+// store created
+const testEntities = {}
+services.map (
+  service => {
+    testEntities[service] = {
+      issues: [],
+      comments: [],
+    }
+  }
+)
+
 describe('getProjectIssues', async () => {
   it ('returns only issues since given timestamp', async () => {
     await Promise.all (services.map (async (service) => {
       const issueA = webhookHandler.generateRandomIssue (service)
       const issueB = webhookHandler.generateRandomIssue (service)
 
+      // create one issue before timestamp
       await webhookHandler.createIssue (issueA, service)
       await helpers.asyncTimeout (5000)
       const sinceTimestamp = new Date ().getTime ()
+      // create another after the timestamp
       await webhookHandler.createIssue (issueB, service)
-
-      randomIssues.push (issueA, issueB)
 
       const issues = await webhookHandler.getProjectIssues (service, sinceTimestamp)
       expect (issues.length).to.equal (1)
+
+      testEntities[service].issues.push (issueA)
+      testEntities[service].issues.push (issueB)
     }))
   })
 })
 
-describe('initDoMirroring', async () => {
+describe('initDoMirroring', async () => {  
   it ('mirrors issues', async () => {
     // check that there are expected number of issues before mirroring
     await Promise.all (services.map (async (service) => {
@@ -65,11 +77,74 @@ describe('initDoMirroring', async () => {
       expect (issues.length).to.equal (2)
     }))
 
+    // do mirroring
     await webhookHandler.initDoMirroring ({testTimestamp})
 
     await Promise.all (services.map (async (service) => {
       const issues = await webhookHandler.getProjectIssues (service, testTimestamp)
+
+      // expect originals (2) and mirrors from other service (2)
       expect (issues.length).to.equal (4)
+
+      // test existence of mirrors
+      const mirrorIssues = issues.filter ((issue) => !webhookHandler.getIsOriginal (issue))
+      expect (mirrorIssues.length).to.equal (2)
     }))
   })
+
+  it ('creates comments', async () => {
+    await Promise.all (services.map (async (service) => {
+      const commentA = await webhookHandler.generateRandomComment (service)
+      const commentB = await webhookHandler.generateRandomComment (service)
+
+      const issues = await webhookHandler.getProjectIssues (service, testTimestamp)
+
+      // comment on first issue
+      const parentIssueId = issues[0].id
+      await webhookHandler.createComment (commentA, service, parentIssueId)
+      await webhookHandler.createComment (commentB, service, parentIssueId)
+
+      const issueComments = await webhookHandler.getComments (service, parentIssueId)
+
+      // test comment creation
+      expect (issueComments.length).to.equal (2)
+      // test comment equality
+      expect (issueComments[0].body).to.equal (commentA.body)
+      expect (issueComments[1].body).to.equal (commentB.body)
+
+      // add issueId
+      commentA.issueId = parentIssueId
+      commentB.issueId = parentIssueId
+
+      // store test comments
+      testEntities[service].comments.push (commentA)
+      testEntities[service].comments.push (commentB)
+    }))
+  })
+
+  it ('mirrors comments', async () => {
+    await webhookHandler.initDoMirroring ({testTimestamp})
+
+    await Promise.all (services.map (async (service) => {
+      const issues = await webhookHandler.getProjectIssues (service, testTimestamp)
+
+      for (let i = 0; i < issues.length; ++i) {
+        const issue = issues[i]
+
+        if (webhookHandler.getIsOriginal (issue)) {
+          await Promise.all (services.map (async (service2) => {
+            if (service === service2)
+              return
+
+            // get mirror issue id
+            // get comments from original
+            // get comments from mirror
+            // compare them
+            // const issueComments = await webhookHandler.getComments (service, mirrorIssues[0].id)
+          }))
+        }
+      }
+    }))
+  })
+
 })
